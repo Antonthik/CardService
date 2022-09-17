79fc1f2ad3eb9d4d79c20418fa19ea8091b48561
antonchik <ganseanderson@gmail.com>
2022-09-17 22:05:10 +0300

    les1 Установка логирования Nlog

---
+++ CardStorService/Controllers/CardController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardStorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        #region Service
+        private readonly ILogger<CardController> _logger;
+        #endregion
+
+        #region Constructor
+        public CardController(ILogger<CardController> logger)
+        {
+            _logger = logger;
+        }
+        #endregion
+
+        #region Public Methods
+
+        [HttpGet("getAll")]
+        public IActionResult GetByclientId(string clientId)
+        {
+            _logger.LogInformation("GetByIdClient.......");
+            return Ok();
+        }
+        #endregion
+    }
+}
--- CardStorService/Program.cs
+++ CardStorService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.HttpLogging;
+using NLog.Web;
+
 namespace CardStorService
 {
     public class Program
@@ -6,7 +9,36 @@ namespace CardStorService
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container
+            
+            
+ 
+            #region Logging Service
+
+            //Настройка логгера - ограничения на вывод сообщения и ограничения размера файла
+            // и обработка всех http запросов
+            builder.Services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.RequestHeaders.Add("Authorization");
+                logging.RequestHeaders.Add("X-Real-IP");
+                logging.RequestHeaders.Add("X-Forwarded-For");
+            });
+
+            // В этом блоке стандартная настройка логгера
+            builder.Host.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+
+            }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
+
+            #endregion
+
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,6 +55,7 @@ namespace CardStorService
             }
 
             app.UseAuthorization();
+            app.UseHttpLogging(); //17.09.2022
 
             app.MapControllers();
 
